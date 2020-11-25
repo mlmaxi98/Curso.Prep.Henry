27239443c10d7784e28757f0292df684fba7eee1
Joaquín Cardozo <cardozojoaquinm@gmail.com>
2020-11-25 15:11:38 -0300

    HNW 6 and 7 ready

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,14 +8,26 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-
+  function Usuario(opciones) {
+    this.usuario = opciones['usuario'];
+    this.nombre = opciones['nombre'];
+    this.email = opciones['email'];
+    this.password = opciones['password'];
+  }     
+  Usuario.prototype.saludar = function() {
+      return 'Hola, mi nombre es '+this.nombre;
+  }
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
-  // Tu código:
-
+  // Tu código: 
+  Constructor.prototype.saludar = function() {
+      return "Hello World!";    
+  }
+  return Constructor;
 }
 
 function agregarStringInvertida() {
@@ -24,9 +36,16 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  str = String.name;
+  String.prototype.reverse = function() { 
+    var aux = this;   
+    var str = '';
+    for (var i = aux.length -1 ; i >= 0; i--) {
+        str += aux[i];
+    }
+    return str;  
 }
-
+}
 // No modificar nada debajo de esta línea
 // --------------------------------
 
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,36 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  return cb() ;  
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  aux=0;
+  for (let i = 0; i < numeros.length; i++) {
+    aux += numeros[i];
+  }
+  cb(aux);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach(element => cb(element))
 }
 
 function map(array, cb) {
   // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb`
+  // y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  nuevoArray = [];
+  nuevoArray = array.map(cb);
+  if (nuevoArray.length == array.length){
+    return nuevoArray;
+  } 
 }
 
 // No modificar nada debajo de esta línea
